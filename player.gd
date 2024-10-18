8479c52cbef5a1d4a0b673e8bb6275629cc46189
johans3p <johan.rochard@S3pweb.com>
2024-10-18 10:46:04 +0200

    feat(starter): demo starter

--- player.gd
+++ player.gd
@@ -2,9 +2,9 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-
+const JUMP_VELOCITY = 7.5
+  
+ 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
